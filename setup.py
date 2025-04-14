9844b34471bda8fd2be9ca05c855dfd2b7767f7a
Mathieu Sanders <736803773+MathieuSanders@users.noreply.github.com>
2025-04-14 10:21:27 -0400

    Update setup.py to allow for package install.

--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
-# Setup configuration for LiftOCR
+from setuptools import setup, find_packages
+
+setup(
+    name="LiftOCR",
+    version="0.1.0",
+    author="Mathieu Sanders",
+    description="A modular, open-source OCR framework for clean architecture and easy expansion.",
+    packages=find_packages(exclude=["tests", "data"]),
+    include_package_data=True,
+    install_requires=[
+        "opencv-python",
+        "PyYAML"
+    ],
+    python_requires='>=3.8',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
